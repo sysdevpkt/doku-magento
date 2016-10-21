6974ed254a8c6cceda0c7fb9182b1c5d8b7e31ee
Adiwinatha Leoputra <azizmudepz@gmail.com>
2016-10-21 16:43:43 +0700

    add fancybox

--- MerchantHosted/view/frontend/web/js/view/payment/method-renderer/oco-method.js
+++ MerchantHosted/view/frontend/web/js/view/payment/method-renderer/oco-method.js
@@ -12,6 +12,51 @@ define(
     function (Component, $) {
         'use strict';
 
+        var formDefault = '<ul>' +
+            '<div doku-div="form-payment">' +
+            '<li>' +
+            '<div class="styled-input fleft width50" id="cc-field">' +
+            '<label>CC</label>' +
+            '</div>' +
+            '<div class="styled-input fright width50" id="cvv-field">' +
+            '<label>CVV</label>' +
+            '</div>' +
+            '<div class="clear"></div>' +
+            '</li>' +
+            '<li>' +
+            '<div class="styled-input fleft width50" id="name-field">' +
+            '<label>NAME</label>' +
+            '</div>' +
+            '<div class="styled-input fright width50" id="exp-field">' +
+            '<label>EXP</label>' +
+            '</div>' +
+            '<div class="clear"></div>' +
+            '</li>' +
+            '</div>' +
+            '<li>' +
+            '<div class="styled-input fleft width50">' +
+            '<input type="text" name="email_cc" id="email_cc" required />' +
+            '<label>Email</label>' +
+            '</div>' +
+            '<div class="styled-input fright width50">' +
+                '<input type="text" name="phone_cc" id="phone_cc" required />' +
+            '<label>Phone</label>' +
+            '</div>' +
+            '<div class="clear"></div>' +
+                '</li>' +
+                '<li>' +
+                '<div class="styled-input fleft width50">' +
+                '<input type="text" name="address" id="address" required />' +
+            '<label>Billing Address</label>' +
+            '</div>' +
+            '<div class="styled-input fright width50">' +
+                '<input type="text" name="id_number" id="id_number" required />' +
+            '<label>ID Number</label>' +
+            '</div>' +
+            '<div class="clear"></div>' +
+                '</li>' +
+                '</ul>';
+
         return Component.extend({
             defaults: {
                 template: 'Doku_MerchantHosted/payment/oco'
@@ -23,12 +68,34 @@ define(
             },
 
             initObservable: function() {
-
+                $.getScript("https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.pack.js", function() {});
+                $.getScript("https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css", function() {});
                 $.getScript("https://staging.doku.com/doku-js/assets/js/doku.js?version="+ new Date().getTime(), function() {});
+                $.getScript("https://staging.doku.com/doku-js/assets/css/doku.css", function() {});
                 return this;
             },
 
             initDoku: function(){
+
+                $.fancybox.open([
+                    {
+                        closeClick: false,
+                        type: 'iframe',
+                        openEffect: 'fade',
+                        closeEffect: 'fade',
+                        openSpeed: 'slow',
+                        closeSpeed: 'slow',
+                        content: formDefault,
+                        closeBtn: false,
+                        autoResize: true,
+                        helpers : {
+                            overlay : {
+                                closeClick : false
+                            }
+                        }
+                    }
+
+                ]);
 
                 var data = new Object();
 

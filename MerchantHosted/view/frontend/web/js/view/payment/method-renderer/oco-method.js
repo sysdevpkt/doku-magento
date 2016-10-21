4c0ef0ee193f1f475e970b35bf45dc4107761892
Adiwinatha Leoputra <azizmudepz@gmail.com>
2016-10-21 17:24:37 +0700

    debug 7

--- MerchantHosted/view/frontend/web/js/view/payment/method-renderer/oco-method.js
+++ MerchantHosted/view/frontend/web/js/view/payment/method-renderer/oco-method.js
@@ -12,8 +12,6 @@ define(
     function (Component, $) {
         'use strict';
 
-        console.log('panggil 4');
-
         return Component.extend({
             defaults: {
                 template: 'Doku_MerchantHosted/payment/oco'
@@ -26,7 +24,23 @@ define(
 
             initObservable: function() {
 
-                console.log('panggil 5');
+                var data = new Object();
+
+                data.req_merchant_code = '2074'; //mall id or merchant id
+                data.req_chain_merchant = 'NA'; //chain merchant id
+                data.req_payment_channel = '15'; //payment channel
+                data.req_basket = '';
+                data.req_server_url = 'http://crm.doku.com/doku-library-staging/example-payment/merchant-example.php'; //merchant payment url to receive pairing code & token
+                data.req_transaction_id = 'invoice_1477040126'; //invoice no
+                data.req_amount = '10000.00';
+                data.req_currency = '360'; //360 for IDR
+                data.req_words = 'f2be9d27c3ce8b01eba427f1b08c399a0d5051ac'; //your merchant unique key
+                data.req_session_id = '1477040127626'; //your server timestamp
+                data.req_form_type = 'inline';
+                data.req_custom_form = ['cc-field', 'cvv-field', 'name-field', 'exp-field'];
+
+                getForm(data);
+
                 return this;
             },
 
--- MerchantHosted/view/frontend/web/js/view/payment/oco.js
+++ MerchantHosted/view/frontend/web/js/view/payment/oco.js
@@ -17,8 +17,6 @@ define(
     ) {
         'use strict';
 
-        console.log('panggil 1');
-
         rendererList.push(
             {
                 type: 'oco',
@@ -26,8 +24,6 @@ define(
             }
         );
         /** Add view logic here if needed */
-
-        console.log('panggil 2');
 
         return Component.extend({
 
@@ -45,23 +41,6 @@ define(
                     type: "text/css",
                     href: "https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css"
                 });
-
-                var data = new Object();
-
-                data.req_merchant_code = '2074'; //mall id or merchant id
-                data.req_chain_merchant = 'NA'; //chain merchant id
-                data.req_payment_channel = '15'; //payment channel
-                data.req_basket = '';
-                data.req_server_url = 'http://crm.doku.com/doku-library-staging/example-payment/merchant-example.php'; //merchant payment url to receive pairing code & token
-                data.req_transaction_id = 'invoice_1477040126'; //invoice no
-                data.req_amount = '10000.00';
-                data.req_currency = '360'; //360 for IDR
-                data.req_words = 'f2be9d27c3ce8b01eba427f1b08c399a0d5051ac'; //your merchant unique key
-                data.req_session_id = '1477040127626'; //your server timestamp
-                data.req_form_type = 'inline';
-                data.req_custom_form = ['cc-field', 'cvv-field', 'name-field', 'exp-field'];
-
-                //getForm(data);
 
                 return this;
             },

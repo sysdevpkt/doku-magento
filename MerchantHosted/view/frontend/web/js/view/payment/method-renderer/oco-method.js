68fac61bb2485eb8bc6f43a85c9ed16ca2259dfb
Adiwinatha Leoputra <azizmudepz@gmail.com>
2016-10-21 23:19:08 +0700

    add document ready

--- MerchantHosted/view/frontend/web/js/view/payment/method-renderer/oco-method.js
+++ MerchantHosted/view/frontend/web/js/view/payment/method-renderer/oco-method.js
@@ -9,17 +9,9 @@ define(
     function (Component, $) {
         'use strict';
 
-        return Component.extend({
-            defaults: {
-                template: 'Doku_MerchantHosted/payment/oco'
-            },
+        $(document).ready(function(){
 
-            /** Returns send check to info */
-            getMailingAddress: function() {
-                return window.checkoutConfig.payment.checkmo.mailingAddress;
-            },
-
-            initObservable: function() {
+                console.log($("[doku-div='form-payment']").html());
 
                 var data = new Object();
 
@@ -38,7 +30,38 @@ define(
 
                 getForm(data);
 
-                console.log(data.res_form_payment);
+        });
+
+        return Component.extend({
+            defaults: {
+                template: 'Doku_MerchantHosted/payment/oco'
+            },
+
+            /** Returns send check to info */
+            getMailingAddress: function() {
+                return window.checkoutConfig.payment.checkmo.mailingAddress;
+            },
+
+            initObservable: function() {
+
+                // var data = new Object();
+
+                // data.req_merchant_code = '2074'; //mall id or merchant id
+                // data.req_chain_merchant = 'NA'; //chain merchant id
+                // data.req_payment_channel = '15'; //payment channel
+                // data.req_basket = '';
+                // data.req_server_url = 'http://crm.doku.com/doku-library-staging/example-payment/merchant-example.php'; //merchant payment url to receive pairing code & token
+                // data.req_transaction_id = 'invoice_1477040126'; //invoice no
+                // data.req_amount = '10000.00';
+                // data.req_currency = '360'; //360 for IDR
+                // data.req_words = 'f2be9d27c3ce8b01eba427f1b08c399a0d5051ac'; //your merchant unique key
+                // data.req_session_id = '1477040127626'; //your server timestamp
+                // data.req_form_type = 'inline';
+                // data.req_custom_form = ['cc-field', 'cvv-field', 'name-field', 'exp-field'];
+
+                // getForm(data);
+
+                // console.log(data.res_form_payment);
 
                 return this;
             },

99d51bde9a2c2618ecd8976c964da22022e070d3
Adiwinatha Leoputra <azizmudepz@gmail.com>
2016-10-23 15:35:14 +0700

    change when condition

--- MerchantHosted/view/frontend/web/js/view/payment/method-renderer/oco-method.js
+++ MerchantHosted/view/frontend/web/js/view/payment/method-renderer/oco-method.js
@@ -56,7 +56,7 @@ define(
                 data.req_form_type = 'inline';
                 data.req_custom_form = ['cc-field', 'cvv-field', 'name-field', 'exp-field'];
 
-                $.when( { getForm(data) } ).done(function() {
+                $.when(getForm(data)).done(function() {
                     console.log('when');
                     console.log($("#doku-cc-number").val());
                 });

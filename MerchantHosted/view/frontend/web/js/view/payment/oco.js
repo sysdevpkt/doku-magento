84198f499e3becbf1fd840fa815e3627109ca1b6
Adiwinatha Leoputra <azizmudepz@gmail.com>
2016-10-23 17:29:06 +0700

    move load js

--- MerchantHosted/view/frontend/web/js/view/payment/oco.js
+++ MerchantHosted/view/frontend/web/js/view/payment/oco.js
@@ -4,13 +4,11 @@
 define(
     [
         'uiComponent',
-        'Magento_Checkout/js/model/payment/renderer-list',
-        'jquery'
+        'Magento_Checkout/js/model/payment/renderer-list'
     ],
     function (
         Component,
-        rendererList,
-        $
+        rendererList
     ) {
         'use strict';
 
@@ -22,26 +20,6 @@ define(
         );
         /** Add view logic here if needed */
 
-        return Component.extend({
-
-            initObservable: function() {
-
-                $.getScript("https://staging.doku.com/doku-js/assets/js/jquery.payment.min.js", function() {});
-                $.getScript("https://staging.doku.com/doku-js/assets/js/responsive-tabs.js", function() {});
-                $.getScript("https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.pack.js", function() {});
-                $.getScript("https://staging.doku.com/doku-js/assets/js/doku.uncompress.js?version="+ new Date().getTime(), function() {});
-
-                $("head").append("<link>");
-                var css = $("head").children(":last");
-                css.attr({
-                    rel:  "stylesheet",
-                    type: "text/css",
-                    href: "https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css"
-                });
-
-                return this;
-            },
-
-        });
+        return Component.extend({});
     }
 );

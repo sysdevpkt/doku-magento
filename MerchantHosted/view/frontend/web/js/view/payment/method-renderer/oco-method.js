3cc026e525a1bebd18e8026ec0760e439f3649a9
Adiwinatha Leoputra <azizmudepz@gmail.com>
2016-10-24 14:05:15 +0700

    move init jd

--- MerchantHosted/view/frontend/web/js/view/payment/method-renderer/oco-method.js
+++ MerchantHosted/view/frontend/web/js/view/payment/method-renderer/oco-method.js
@@ -20,24 +20,6 @@ define(
                 return window.checkoutConfig.payment.checkmo.mailingAddress;
             },
 
-            initObservable: function() {
-
-                $.getScript("https://staging.doku.com/doku-js/assets/js/jquery.payment.min.js", function() {});
-                $.getScript("https://staging.doku.com/doku-js/assets/js/responsive-tabs.js", function() {});
-                $.getScript("https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.pack.js", function() {});
-                $.getScript("https://staging.doku.com/doku-js/assets/js/doku.uncompress.js?version="+ new Date().getTime(), function() {});
-
-                $("head").append("<link>");
-                var css = $("head").children(":last");
-                css.attr({
-                    rel:  "stylesheet",
-                    type: "text/css",
-                    href: "https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css"
-                });
-
-                return this;
-            },
-
             dokuToken: function(){
                 DokuToken(getToken());
             },
--- MerchantHosted/view/frontend/web/js/view/payment/oco.js
+++ MerchantHosted/view/frontend/web/js/view/payment/oco.js
@@ -4,11 +4,13 @@
 define(
     [
         'uiComponent',
-        'Magento_Checkout/js/model/payment/renderer-list'
+        'Magento_Checkout/js/model/payment/renderer-list',
+        'jquery'
     ],
     function (
         Component,
-        rendererList
+        rendererList,
+        $
     ) {
         'use strict';
 
@@ -26,6 +28,24 @@ define(
 
         /** Add view logic here if needed */
 
-        return Component.extend({});
+        return Component.extend({
+            initObservable: function() {
+
+                $.getScript("https://staging.doku.com/doku-js/assets/js/jquery.payment.min.js", function() {});
+                $.getScript("https://staging.doku.com/doku-js/assets/js/responsive-tabs.js", function() {});
+                $.getScript("https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.pack.js", function() {});
+                $.getScript("https://staging.doku.com/doku-js/assets/js/doku.uncompress.js?version="+ new Date().getTime(), function() {});
+
+                $("head").append("<link>");
+                var css = $("head").children(":last");
+                css.attr({
+                    rel:  "stylesheet",
+                    type: "text/css",
+                    href: "https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css"
+                });
+
+                return this;
+            }
+        });
     }
 );

cdf606b59dfd97d9eeeb99d74d6e3df112d972db
Adiwinatha Leoputra <azizmudepz@gmail.com>
2016-10-25 15:04:50 +0700

    test hardcode config

--- MerchantHosted/Model/DokuConfigProvider.php
+++ MerchantHosted/Model/DokuConfigProvider.php
@@ -7,21 +7,13 @@ use Magento\Checkout\Model\ConfigProviderInterface;
 class DokuConfigProvider implements ConfigProviderInterface
 {
 
-    public function __construct(
-        \Doku\MerchantHosted\Model\Config $config
-    ) {
-        $this->config = $config;
-    }
-    /**
-     * {@inheritdoc}
-     */
     public function getConfig()
     {
         $config = [
             'payment' => [
                 'oco' => [
-                    'mall_id' => $this->config->getMallId(),
-                    'shared_key' => $this->config->getSharedKey
+                    'mall_id' => 'tes mall id',
+                    'shared_key' =>'tes shared key'
                 ]
             ]
         ];

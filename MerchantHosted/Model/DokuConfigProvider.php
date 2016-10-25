e0b6e4da11c4586cd2608d011cf6faa516600c13
Adiwinatha Leoputra <azizmudepz@gmail.com>
2016-10-25 15:33:49 +0700

    testing config

--- MerchantHosted/Model/DokuConfigProvider.php
+++ MerchantHosted/Model/DokuConfigProvider.php
@@ -7,12 +7,18 @@ use Magento\Checkout\Model\ConfigProviderInterface;
 class DokuConfigProvider implements ConfigProviderInterface
 {
 
+    public function __construct(
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+    ){
+        $this->_scopeConfig = $scopeConfig;
+    }
+
     public function getConfig()
     {
         $config = [
             'payment' => [
                 'oco' => [
-                    'mall_id' => 'tes mall id',
+                    'mall_id' => $this->_scopeConfig->getValue('payment/oco/mall_id', \Magento\Store\Model\ScopeInterface::SCOPE_STORE),
                     'shared_key' =>'tes shared key'
                 ]
             ]

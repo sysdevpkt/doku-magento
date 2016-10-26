0ef8e134b256ab470da470b1b20fcc819b14213e
Adiwinatha Leoputra <azizmudepz@gmail.com>
2016-10-26 18:38:18 +0700

    test words

--- MerchantHosted/Controller/Payment/Words.php
+++ MerchantHosted/Controller/Payment/Words.php
@@ -31,20 +31,18 @@ class Words extends \Doku\MerchantHosted\Controller\Payment\Library
     {
 
         $this->logger->info('===== Words Controller ===== Start');
-        $this->logger->info('amount : '. json_encode($this->_session->getQuote()->getBaseGrandTotal(), JSON_PRETTY_PRINT));
-        $this->logger->info('quote id : '. json_encode($this->_session->getQuote(), JSON_PRETTY_PRINT));
-//        $postData = json_decode($_POST['dataWords']);
-//        $this->_logger->info('postdata : '. json_encode($postData, JSON_PRETTY_PRINT));
-//
-//        $params = array(
-//            'amount' => $postData->amount,
-//            'invoice' => $postData->trans_id,
-//            'currency' => $postData->currency
-//        );
+
+        $params = array(
+            'amount' => number_format($this->_session->getQuote()->getBaseGrandTotal(), 2),
+            'invoice' => 'mage2_'. $_POST['_'],
+            'currency' => '360'
+        );
+
+        $words = $this->doCreateWords($params);
 
         $this->logger->info('===== Words Controller ===== End');
 
-        echo true;
+        echo $words;
 
     }
 }

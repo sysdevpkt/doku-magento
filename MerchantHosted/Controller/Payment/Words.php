2dfc8f632e9e500436279d1cab31a9e2ecd52de5
Adiwinatha Leoputra <azizmudepz@gmail.com>
2016-10-26 18:17:36 +0700

    add getquote

--- MerchantHosted/Controller/Payment/Words.php
+++ MerchantHosted/Controller/Payment/Words.php
@@ -32,10 +32,7 @@ class Words extends \Doku\MerchantHosted\Controller\Payment\Library
 
         $this->logger->info('===== Words Controller ===== Start');
         $this->logger->info('amount : '. json_encode($this->_session->getQuote()->getBaseGrandTotal(), JSON_PRETTY_PRINT));
-        $this->logger->info('quote id : '. json_encode($this->_session->getQuoteId(), JSON_PRETTY_PRINT));
-        $this->logger->info('session id : '. json_encode($this->_session->getSessionId(), JSON_PRETTY_PRINT));
-        $this->logger->info('getOrigOrderId : '. json_encode($this->_session->getQuote()->getOrigOrderId(), JSON_PRETTY_PRINT));
-        $this->logger->info('getReservedOrderId : '. json_encode($this->_session->getQuote()->getReservedOrderId(), JSON_PRETTY_PRINT));
+        $this->logger->info('quote id : '. json_encode($this->_session->getQuote(), JSON_PRETTY_PRINT));
 //        $postData = json_decode($_POST['dataWords']);
 //        $this->_logger->info('postdata : '. json_encode($postData, JSON_PRETTY_PRINT));
 //

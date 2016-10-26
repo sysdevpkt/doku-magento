7003d90c4274a73a887799bf9a1d6eb93c894ecd
Adiwinatha Leoputra <azizmudepz@gmail.com>
2016-10-26 11:31:42 +0700

    add words library

--- MerchantHosted/Controller/Payment/Library.php
+++ MerchantHosted/Controller/Payment/Library.php
@@ -7,15 +7,45 @@ use Doku\MerchantHosted\Model\Oco;
 class Order extends \Magento\Framework\App\Action\Action{
 
 	protected $_logger;
+    protected $prePaymentUrl = 'https://staging.doku.com/api/payment/PrePayment';
+    protected $paymentUrl = 'https://staging.doku.com/api/payment/paymentMip';
+    protected $directPaymentUrl = 'https://staging.doku.com/api/payment/PaymentMIPDirect';
+    protected $generateCodeUrl = 'https://staging.doku.com/api/payment/doGeneratePaymentCode';
+    protected $redirectPaymentUrl = 'https://staging.doku.com/api/payment/doInitiatePayment';
+    protected $captureUrl = 'https://staging.doku.com/api/payment/DoCapture';
 
     public function __construct(
         \Psr\Log\LoggerInterface $logger, //log injection
         \Magento\Framework\App\Action\Context $context,
-        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
     ) {
         $this->_logger = $logger;
         parent::__construct($context);
         $this->resultPageFactory = $resultPageFactory;
+        $this->_scopeConfig = $scopeConfig;
+    }
+
+    protected function getMallId(){
+        return $this->_scopeConfig->getValue('payment/oco/mall_id', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+    }
+
+    protected function getSharedKey(){
+        return $this->_scopeConfig->getValue('payment/oco/shared_key', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+    }
+
+    protected function doCreateWords($data){
+        if(!empty($data['device_id']))
+            if(!empty($data['pairing_code']))
+                return sha1($data['amount'] . $this->getMallId() . $this->getSharedKey() . $data['invoice'] . $data['currency'] . $data['token'] . $data['pairing_code'] . $data['device_id']);
+            else
+                return sha1($data['amount'] . $this->getMallId() . $this->getSharedKey() . $data['invoice'] . $data['currency'] . $data['device_id']);
+        else if(!empty($data['pairing_code']))
+            return sha1($data['amount'] . $this->getMallId() . $this->getSharedKey() . $data['invoice'] . $data['currency'] . $data['token'] . $data['pairing_code']);
+        else if(!empty($data['currency']))
+            return sha1($data['amount'] . $this->getMallId() . $this->getSharedKey() . $data['invoice'] . $data['currency']);
+        else
+            return sha1($data['amount'] . $this->getMallId() . $this->getSharedKey() . $data['invoice']);
     }
     
 }
---
+++ MerchantHosted/Controller/Payment/Words.php
@@ -0,0 +1,48 @@
+<?php
+namespace Doku\MerchantHosted\Controller\Payment;
+
+use Doku\MerchantHosted\Model\Oco;
+use Magento\Checkout\Model\Session;
+
+class Words extends \Doku\MerchantHosted\Controller\Payment\Library
+{
+
+    protected $_logger;
+
+    public function __construct(
+        \Psr\Log\LoggerInterface $logger, //log injection
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+        Session $session
+    )
+    {
+        $this->_logger = $logger;
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+        $this->_session = $session;
+    }
+
+    public function execute()
+    {
+
+        $this->_logger->info('===== Words Controller ===== Start');
+        $this->_logger->info('amount : '. json_encode($this->_session->getQuote()->getBaseGrandTotal(), JSON_PRETTY_PRINT));
+        $this->_logger->info('quote id : '. json_encode($this->_session->getQuoteId(), JSON_PRETTY_PRINT));
+        $this->_logger->info('session id : '. json_encode($this->_session->getSessionId(), JSON_PRETTY_PRINT));
+        $this->_logger->info('getOrigOrderId : '. json_encode($this->_session->getQuote()->getOrigOrderId(), JSON_PRETTY_PRINT));
+        $this->_logger->info('getReservedOrderId : '. json_encode($this->_session->getQuote()->getReservedOrderId(), JSON_PRETTY_PRINT));
+//        $postData = json_decode($_POST['dataWords']);
+//        $this->_logger->info('postdata : '. json_encode($postData, JSON_PRETTY_PRINT));
+//
+//        $params = array(
+//            'amount' => $postData->amount,
+//            'invoice' => $postData->trans_id,
+//            'currency' => $postData->currency
+//        );
+
+        $this->_logger->info('===== Words Controller ===== End');
+
+        echo true;
+
+    }
+}

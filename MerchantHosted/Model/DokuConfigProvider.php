b8f797e34e35932c49823690cbf2d59f2d3acf04
Adiwinatha Leoputra <azizmudepz@gmail.com>
2016-10-26 15:28:28 +0700

    add doku config

--- MerchantHosted/Model/DokuConfigProvider.php
+++ MerchantHosted/Model/DokuConfigProvider.php
@@ -7,12 +7,16 @@ use Magento\Checkout\Model\ConfigProviderInterface;
 class DokuConfigProvider implements ConfigProviderInterface
 {
 
+    protected $config;
+
     public function __construct(
         \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
-        \Magento\Store\Model\StoreManagerInterface $storeManager
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Doku\MerchantHosted\Model\DokuConfig $config
     ){
         $this->_scopeConfig = $scopeConfig;
         $this->_storeManager = $storeManager;
+        $this->config = $config;
     }
 
     public function getConfig()
@@ -20,9 +24,8 @@ class DokuConfigProvider implements ConfigProviderInterface
         $config = [
             'payment' => [
                 'oco' => [
-                    'mall_id' => $this->_scopeConfig->getValue('payment/oco/mall_id', \Magento\Store\Model\ScopeInterface::SCOPE_STORE),
+                    'mall_id' => $this->config->getMallId(),
                     'shared_key' => $this->_scopeConfig->getValue('payment/oco/shared_key', \Magento\Store\Model\ScopeInterface::SCOPE_STORE),
-                    'currency' => $this->_storeManager->getStore()->getCurrentCurrencyCode()
                 ]
             ]
         ];

946bdd879cb92f1c47d7ad2a285f5830c7b7bd5e
Adiwinatha Leoputra <azizmudepz@gmail.com>
2016-10-27 10:45:18 +0700

    test quote

--- MerchantHosted/Controller/Payment/Words.php
+++ MerchantHosted/Controller/Payment/Words.php
@@ -5,17 +5,26 @@ use Magento\Checkout\Model\Session;
 use \Psr\Log\LoggerInterface;
 use \Magento\Framework\App\Action\Context;
 use Doku\MerchantHosted\Model\DokuConfigProvider;
+use Magento\Checkout\Block\Cart\AbstractCart;
+use \Magento\Checkout\Block\Cart\Totals;
+use \Magento\Checkout\Helper\Cart;
 
 class Words extends \Doku\MerchantHosted\Controller\Payment\Library
 {
 
     protected $session;
+    protected $cart;
+    protected $totals;
+    protected $carts;
 
     public function __construct(
         LoggerInterface $logger, //log injection
         Context $context,
         DokuConfigProvider $config,
-        Session $session
+        Session $session,
+        AbstractCart $cart,
+        Totals $totals,
+        Cart $carts
     )
     {
         parent::__construct(
@@ -26,6 +35,9 @@ class Words extends \Doku\MerchantHosted\Controller\Payment\Library
         );
 
         $this->session = $session;
+        $this->cart = $cart;
+        $this->totals = $totals;
+        $this->carts = $carts;
     }
 
     public function execute()
@@ -45,9 +57,13 @@ class Words extends \Doku\MerchantHosted\Controller\Payment\Library
             );
 
             $this->logger->info('params : '. json_encode($params, JSON_PRETTY_PRINT));
-            $this->logger->info('has items : '. json_encode($this->session->getQuote()->hasItems(), JSON_PRETTY_PRINT));
-            $this->logger->info('stored data : '. json_encode($this->session->getQuote()->getStoredData(), JSON_PRETTY_PRINT));
-            $this->logger->info('stored data : '. json_encode($this->session->getQuote()->convertToArray(), JSON_PRETTY_PRINT));
+            $this->logger->info('session : '. json_encode($this->session->getQuote()->convertToArray(), JSON_PRETTY_PRINT));
+            $this->logger->info('cart : '. json_encode($this->cart->getItems(), JSON_PRETTY_PRINT));
+            $this->logger->info('cart : '. json_encode($this->cart->getQuote()->convertToArray(), JSON_PRETTY_PRINT));
+            $this->logger->info('total : '. json_encode($this->totals->getItems()), JSON_PRETTY_PRINT));
+            $this->logger->info('total : '. json_encode($this->totals->toArray()), JSON_PRETTY_PRINT));
+            $this->logger->info('carts : '. json_encode($this->carts->getCart()->convertToArray()), JSON_PRETTY_PRINT));
+            $this->logger->info('carts : '. json_encode($this->carts->getQuote()->convertToArray()), JSON_PRETTY_PRINT));
 
             $words = $this->doCreateWords($params);
             $arr = array(

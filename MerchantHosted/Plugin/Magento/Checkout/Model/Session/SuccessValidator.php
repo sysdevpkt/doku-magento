852c791b4e2d0fa3c8325364afac5d815fa81d7b
Adiwinatha Leoputra <azizmudepz@gmail.com>
2016-11-04 11:28:16 +0700

    testing

--- MerchantHosted/Plugin/Magento/Checkout/Model/Session/SuccessValidator.php
+++ MerchantHosted/Plugin/Magento/Checkout/Model/Session/SuccessValidator.php
@@ -5,21 +5,29 @@ namespace Doku\MerchantHosted\Plugin\Magento\Checkout\Model\Session;
 use Magento\Sales\Model\Order;
 use Psr\Log\LoggerInterface;
 use Magento\Checkout\Model\Session;
+use \Magento\Quote\Api\CartRepositoryInterface;
+use \Magento\Quote\Model\QuoteRepository;
 
 class SuccessValidator
 {
     protected $session;
     protected $order;
     protected $logger;
+    protected $quote;
+    protected $quote2;
 
     public function __construct(
         Session $session,
         LoggerInterface $logger,
-        Order $order
+        Order $order,
+        CartRepositoryInterface $quote,
+        QuoteRepository $quoteRepository
     ) {
         $this->session = $session;
         $this->logger = $logger;
         $this->order = $order;
+        $this->quote = $quote;
+        $this->quote2 = $quoteRepository;
     }
 
     public function afterIsValid(\Magento\Checkout\Model\Session\SuccessValidator $successValidator, $returnValue)
@@ -27,6 +35,9 @@ class SuccessValidator
         $this->logger->info('masuk');
         $this->logger->info('session : '. json_encode($this->session->getLastRealOrder()->convertToArray(), JSON_PRETTY_PRINT));
         $this->logger->info('order : '. json_encode($this->order->loadByIncrementId($this->session->getLastRealOrder()->getIncrementId())->convertToArray(), JSON_PRETTY_PRINT));
+        $this->logger->info('quote : '. json_encode($this->quote->get($this->session->getLastRealOrder()->getQuoteId()), JSON_PRETTY_PRINT));
+        $this->logger->info('quote2 : '. json_encode($this->quote2->get($this->session->getLastRealOrder()->getQuoteId())->convertToArray(), JSON_PRETTY_PRINT));
+
 
         return $returnValue;
     }

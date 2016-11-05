e9e60a97ff1376a51049f8c7f3378a0e85d06242
Adiwinatha Leoputra <azizmudepz@gmail.com>
2016-11-05 18:01:55 +0700

    add data to quote

--- MerchantHosted/Plugin/Magento/Checkout/Model/Session/SuccessValidator.php
+++ MerchantHosted/Plugin/Magento/Checkout/Model/Session/SuccessValidator.php
@@ -31,9 +31,10 @@ class SuccessValidator
 
         try{
 
-            $this->logger->info('manual session : '. json_encode($this->session->getData('payment_channel'), JSON_PRETTY_PRINT));
             $this->logger->info('session : '. json_encode($this->session->getLastRealOrder()->convertToArray(), JSON_PRETTY_PRINT));
             $this->logger->info('order : '. json_encode($this->order->loadByIncrementId($this->session->getLastRealOrder()->getIncrementId())->convertToArray(), JSON_PRETTY_PRINT));
+            $this->logger->info('quote : '. json_encode($this->quote->get($this->session->getLastRealOrder()->getQuoteId())));
+            $this->logger->info('manual : '. $this->quote->get($this->session->getLastRealOrder()->getQuoteId())->getData('payment_channel'));
 
         }catch(\Exception $e){
             $this->logger->info('error : '. $e->getMessage());

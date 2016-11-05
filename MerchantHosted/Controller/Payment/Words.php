94dead4e71c25ea355ed3cadd61a28517ee32cba
Adiwinatha Leoputra <azizmudepz@gmail.com>
2016-11-05 17:12:36 +0700

    get quotes

--- MerchantHosted/Controller/Payment/Words.php
+++ MerchantHosted/Controller/Payment/Words.php
@@ -35,9 +35,7 @@ class Words extends \Doku\MerchantHosted\Controller\Payment\Library
     {
 
         $this->logger->info('===== Words Controller ===== Start');
-        $this->session->setData('payment_channel', '15');
-        $this->logger->info('isi session : '. json_decode($this->session->getData('payment_channel'), JSON_PRETTY_PRINT));
-
+        $this->logger->info('isi session : '. json_decode($this->session->getQuote()->convertToArray(), JSON_PRETTY_PRINT));
 
         try{
 

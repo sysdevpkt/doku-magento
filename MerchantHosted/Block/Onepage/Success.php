ca32e6d5cb9e288242904949a5c0c987bee01dd0
Adiwinatha Leoputra <azizmudepz@gmail.com>
2016-11-06 16:05:08 +0700

    testing

--- MerchantHosted/Block/Onepage/Success.php
+++ MerchantHosted/Block/Onepage/Success.php
@@ -1,70 +1,12 @@
 <?php
-
 namespace Doku\MerchantHosted\Block\Onepage;
 
-use \Magento\Checkout\Model\Session;
-use \Magento\Sales\Model\Order\Config;
-use Magento\Framework\App\ResourceConnection;
-use \Psr\Log\LoggerInterface;
+use \Magento\Framework\View\Element\Template;
 
-class Success extends \Magento\Checkout\Block\Onepage\Success {
-
-    protected $logger;
-    protected $resourceConnection;
-
-    public function __construct(
-
-        \Magento\Framework\View\Element\Template\Context $context,
-        Session $checkoutSession,
-        Config $orderConfig,
-        \Magento\Framework\App\Http\Context $httpContext,
-        array $data = [],
-        ResourceConnection $resourceConnection,
-        LoggerInterface $logger
-
-    ) {
-        parent::__construct(
-            $context,
-            $checkoutSession,
-            $orderConfig,
-            $httpContext,
-            $data
-        );
-
-        $this->resourceConnection = $resourceConnection;
-        $this->logger = $logger;
-
+class Success extends Template
+{
+    public function getSomething()
+    {
+        return 'returned something from custom block.';
     }
-
-    public function getOrder() {
-        return $this->_checkoutSession->getLastRealOrder();
-    }
-
-    public function getDokuOrder(){
-
-        $order = $this->getOrder();
-
-        $getOrder = $this->resourceConnection->getConnection()->select()->from('doku_orders')
-            ->where('quote_id=?', $order->getQuoteId())->where('store_id=?', $order->getStoreId());
-        $findOrder = $this->resourceConnection->getConnection()->fetchRow($getOrder);
-
-        return $findOrder;
-
-    }
-
-    public function checkPaymentChannel(){
-
-        $this->logger->info('masuk');
-        $order = $this->getDokuOrder();
-
-        if($order['payment_channel_id'] != '04' && $order['payment_channel_id'] != '15'){
-            $this->logger->info('masuk true');
-            return true;
-        }
-        else{
-            $this->logger->info('masuk false');
-            return false;
-        }
-    }
-
 }

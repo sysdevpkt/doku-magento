bbfdd95d79de58f1da65df2cc2f961db92a019d0
Adiwinatha Leoputra <azizmudepz@gmail.com>
2017-01-04 15:21:13 +0700

    testing notify

--- MerchantHosted/Controller/Api/Notify.php
+++ MerchantHosted/Controller/Api/Notify.php
@@ -6,16 +6,20 @@ use \Psr\Log\LoggerInterface;
 use \Magento\Framework\App\Action\Context;
 use Doku\MerchantHosted\Model\DokuConfigProvider;
 use Magento\Framework\App\ResourceConnection;
+use Magento\Sales\Model\OrderRepository;
+use Magento\Sales\Model\Order;
 
 class Notify extends Library{
 
     protected $resourceConnection;
+    private $orderRepository;
 
     public function __construct(
         LoggerInterface $logger,
         Context $context,
         DokuConfigProvider $config,
-        ResourceConnection $resourceConnection
+        ResourceConnection $resourceConnection,
+        OrderRepository $orderRepository
     )
     {
         parent::__construct(
@@ -25,6 +29,7 @@ class Notify extends Library{
         );
 
         $this->resourceConnection = $resourceConnection;
+        $this->orderRepository = $orderRepository;
     }
 
     public function execute()
@@ -34,6 +39,48 @@ class Notify extends Library{
         try{
 
             $this->logger->info('post : '. json_encode($_POST, JSON_PRETTY_PRINT));
+
+            $postData = $_POST;
+            $words = sha1($postData['AMOUNT'] . $this->config->getMallId() . $this->config->getSharedKey()
+                . $postData['TRANSIDMERCHANT'] . $postData['RESULTMSG'] . $postData['VERIFYSTATUS']);
+
+            $this->logger->info('===== Notify Controller ===== Checking words...');
+
+            if($postData['WORDS'] == $words){
+
+                $this->logger->info('===== Notify Controller ===== Checking done');
+                $this->logger->info('===== Notify Controller ===== Finding order...');
+
+                $getOrder = $this->resourceConnection->getConnection()->select()->from('doku_orders')
+                    ->where('invoice_no=?', $postData['TRANSIDMERCHANT']);
+                $findOrder = $this->resourceConnection->getConnection()->fetchRow($getOrder);
+
+                $this->logger->info('===== Notify Controller ===== Order found');
+                $this->logger->info('===== Notify Controller ===== Updating order...');
+
+                $order = $this->orderRepository->get($findOrder['order_id']);
+                $order->setState(Order::STATE_PROCESSING);
+                $order->setStatus(Order::STATE_PROCESSING);
+
+                if($this->orderRepository->save()){
+                    $this->resourceConnection->getConnection()->update('doku_orders',
+                        ['order_status' => 'SUCCESS'], ['invoice_no=?' => $postData['TRANSIDMERCHANT']]);
+
+                    $this->logger->info('===== Notify Controller ===== Updating success...');
+                    echo 'CONTINUE';
+
+                }else{
+
+                    $this->logger->info('===== Notify Controller ===== Updating failed...');
+                    echo 'STOP';
+
+                }
+
+            }else{
+                $this->logger->info('===== Notify Controller ===== Words not match!');
+                echo 'STOP';
+            }
+
             $this->logger->info('===== Notify Controller ===== End');
 
         }catch(\Exception $e){

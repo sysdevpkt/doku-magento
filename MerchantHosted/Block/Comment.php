61dfc3728a696d52c139bcf9ed9464b170af4c77
Adiwinatha Leoputra <azizmudepz@gmail.com>
2017-01-15 18:55:38 +0700

    testing custom sales order view

---
+++ MerchantHosted/Block/Comment.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Doku\MerchantHosted\Block;
+
+   class Comment extends \Magento\Framework\View\Element\Template
+   {
+       public function __construct(
+           \Magento\Framework\View\Element\Template\Context $context,
+           array $data = []
+       ) {
+           parent::__construct($context, $data);
+       }
+
+
+   }

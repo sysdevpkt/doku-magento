d88980ba38d9138e562da46dc28a8072b471fd95
Adiwinatha Leoputra <azizmudepz@gmail.com>
2017-03-31 16:10:42 +0700

    add config

---
+++ MerchantHosted/Model/Payment/SinarmasFull.php
@@ -0,0 +1,12 @@
+<?php
+namespace Doku\MerchantHosted\Model\Payment;
+
+/**
+ * Pay In Store payment method model
+ */
+class SinarmasFull extends \Magento\Payment\Model\Method\AbstractMethod
+{
+
+    protected $_code = 'sinarmas_full';
+
+}

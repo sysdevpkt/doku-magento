1e306d432f3e4fdcff9d947820e308950adeffe3
rudythong <rudy.thong@gmail.com>
2017-06-10 10:21:57 +0700

    Fixing adding Mandiri VA to input prefix

---
+++ MerchantHosted/Model/Payment/MandiriVa.php
@@ -0,0 +1,12 @@
+<?php
+namespace Doku\MerchantHosted\Model\Payment;
+
+/**
+ * Pay In Store payment method model
+ */
+class MandiriVa extends \Magento\Payment\Model\Method\AbstractMethod
+{
+
+    protected $_code = 'mandiri_va';
+
+}
